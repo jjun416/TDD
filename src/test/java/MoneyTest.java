e386cadb78f6d6b56ab709785e0fb6493497ebdb
jjun416 <lipsguswn@gmail.com>
2015-12-09 23:50:35 +0900

    2장 타락한 객체

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -1,19 +1,21 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/*
+*  TODO 리스트업하기
+* */
+
+
 public class MoneyTest {
     @Test
     public void testMultiplication() {
+
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10, product.amount);
+        assertEquals(10,product.amount);
+
         product = five.times(3);
-        assertEquals(15, product.amount);
-    }
-    //test
-    @Test
-    public void testEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertEquals(15,product.amount);
+
     }
 }
